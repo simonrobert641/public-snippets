3e20f2dc00fd025da3554906f4bf6b2c4f4ebe08
Simon Robert <simon.r@allopneus.com>
2017-11-13 11:06:09 +0100

    Dev EXTRACT & arguments curl

--- bashTests.sh
+++ bashTests.sh
@@ -11,14 +11,17 @@ _myCurl="curl -v -k -L -c $cookiesFile -b $cookiesFile"
 rm -f "$cookiesFile"
 
 function GET {
-    echo -n "GET $1 :"
-    $_myCurl "$*" &> "$OUT" &&
+    url="$1"
+    shift
+    echo -n "GET $url :"
+    $_myCurl "$url" "$@" &> "$OUT" &&
     { echo -e "\e[0;49;92m OK \e[0m"; true; } ||
     { echo -e " \e[7;49;91m FAIL \e[0m"; R=1; cp -v "$OUT" "$OUT.$(date +%Y-%m-%d_%H-%M-%S)"; false; }
 }
 
 function POST {
-    echo -n "POST $1 :"
+    url="$1"
+    echo -n "POST $url :"
     local i=0 args=()
     while read line; do
         args[$i]="--data-urlencode"
@@ -28,7 +31,22 @@ function POST {
     done < <(cat)
 
     test "$DEBUG" -eq 0 || set -x
-    $_myCurl "$1" -X POST "${args[@]}" &> "$OUT"
+
+    shift
+    $_myCurl "$url" -X POST "${args[@]}" "$@" &> "$OUT"
+    local r=$?
+    test "$DEBUG" -eq 0 || set +x;
+    test "$r" -eq 0 &&
+    { echo -e "\e[0;49;92m OK \e[0m"; true; } ||
+    { echo -e " \e[7;49;91m FAIL \e[0m"; R=1; cp -v "$OUT" "$OUT.$(date +%Y-%m-%d_%H-%M-%S)"; false; }
+}
+
+function RAWPOST {
+    url="$1"
+    shift
+    echo -n "POST $url :"
+    test "$DEBUG" -eq 0 || set -x
+    $_myCurl "$url" -X POST "${args[@]}" "$@" --data-binary - &> "$OUT"
     local r=$?
     test "$DEBUG" -eq 0 || set +x;
     test "$r" -eq 0 &&
@@ -61,6 +79,7 @@ function SHELL {
 function EXTRACT {
     re="$1"
     subs="$2"
+    test "$subs" = "" && subs='$1'
     cat "$OUT" | perl -n0e "\$i=\$_; s|^.*?$re.*$|$subs|s; print if \$i =~ m|$re|s"
 }
 
